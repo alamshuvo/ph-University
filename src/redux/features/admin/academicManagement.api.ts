7b560ac5651227e7e0bc3a259e5ea4ad56220b58
alamshuvo <iftakharalamshuvo11@gmail.com>
2025-01-28 23:36:38 +0600

    get all semester added

--- src/redux/features/admin/academicManagement.api.ts
+++ src/redux/features/admin/academicManagement.api.ts
@@ -1,22 +1,29 @@
 import { baseApi } from "../../api/baseApi";
 
 const academicManagement = baseApi.injectEndpoints({
-    endpoints:(builder)=>({
-        getAllSemester: builder.query({
-            query:()=>({
-                url:"/academic-semesters",
-                method:"GET",
-            })
-        }),
-        addAcademicSemester: builder.mutation({
-            query:(data)=>({
-                url:"/academic-semesters/create-academic-semesters",
-                method:"POST",
-                body:data
-            })
-        })
-        
-    })
-})
+  endpoints: (builder) => ({
+    getAllSemester: builder.query({
+      query: () => ({
+        url: "/academic-semesters",
+        method: "GET",
+      }),
+      transformErrorResponse: (response) => {
+        console.log("inside redux", response);
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+    addAcademicSemester: builder.mutation({
+      query: (data) => ({
+        url: "/academic-semesters/create-academic-semesters",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
 
-export const {useGetAllSemesterQuery,useAddAcademicSemesterMutation} = academicManagement
+export const { useGetAllSemesterQuery, useAddAcademicSemesterMutation } =
+  academicManagement;
